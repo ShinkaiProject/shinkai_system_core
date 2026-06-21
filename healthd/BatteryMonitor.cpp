7014e1b3df92724e402b531644297ef4e1c217a1
xyyx <xyyx@mail.ru>
2026-06-21 17:10:36 +0000

    core: Lockscreen Charging (2/3)
    Change-Id: I71f554814b0af161fbbf18248c84a36017881160
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -50,6 +50,8 @@
 #define POWER_SUPPLY_SYSFS_PATH "/sys/class/" POWER_SUPPLY_SUBSYSTEM
 #define DEV_GLOB "/sys/devices/platform/*gcdd/system_dev_stat"
 #define CDD_SYSTEM_DEVICE_TEMP 8
+#define SYSFS_BATTERY_CURRENT "/sys/class/power_supply/battery/current_now"
+#define SYSFS_BATTERY_VOLTAGE "/sys/class/power_supply/battery/voltage_now"
 #define FAKE_BATTERY_CAPACITY 42
 #define FAKE_BATTERY_TEMPERATURE 424
 #define MILLION 1.0e6
@@ -604,24 +606,16 @@ void BatteryMonitor::updateValues(void) {
                 default:
                     KLOG_WARNING(LOG_TAG, "%s: Unknown power supply type\n", chargerName.c_str());
             }
-            path.clear();
-            path.appendFormat("%s/%s/current_max", POWER_SUPPLY_SYSFS_PATH, chargerName.c_str());
-            int ChargingCurrent = tryGetIntField(path).value_or(0);
 
-            int ChargingVoltage;
-            path.clear();
-            path.appendFormat("%s/%s/voltage_max", POWER_SUPPLY_SYSFS_PATH, chargerName.c_str());
-            if (auto vmax = tryGetIntField(path); vmax.ok() || vmax.error().code() != ENOENT) {
-                ChargingVoltage = vmax.value_or(0);
-            } else {
-                path.clear();
-                path.appendFormat("%s/%s/voltage_max_design", POWER_SUPPLY_SYSFS_PATH,
-                                  chargerName.c_str());
-                if (auto vmax = tryGetIntField(path); vmax.ok() || vmax.error().code() != ENOENT)
-                    ChargingVoltage = vmax.value_or(0);
-                else
-                    ChargingVoltage = DEFAULT_VBUS_VOLTAGE;
-            }
+            int ChargingCurrent =
+                  (access(SYSFS_BATTERY_CURRENT, R_OK) == 0)
+                          ? abs(tryGetIntField(String8(SYSFS_BATTERY_CURRENT)).value_or(0))
+                          : 0;
+
+            int ChargingVoltage =
+                   (access(SYSFS_BATTERY_VOLTAGE, R_OK) == 0)
+                           ? tryGetIntField(String8(SYSFS_BATTERY_VOLTAGE)).value_or(0)
+                           : DEFAULT_VBUS_VOLTAGE;
 
             double power =
                     ((double)ChargingCurrent / MILLION) * ((double)ChargingVoltage / MILLION);
